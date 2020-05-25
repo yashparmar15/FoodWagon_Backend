e5c5e85748f6dcb52102c42aaeeda4d8370f43ec
Yash Parmar <yashparmar15700@gmail.com>
2020-05-25 21:07:42 +0530

    Restaurent Outlet Done

--- FoodWagon/settings.py
+++ FoodWagon/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Foodwagon',
         'USER': 'postgres',
-        'PASSWORD': 'P@ssw0rd',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -130,3 +130,12 @@ MEDIA_ROOT = 'media'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'yashparmar157000@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
--- foodwagon_backend/views.py
+++ foodwagon_backend/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from foodwagon_backend.models import Venues,Trucks
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.mail import send_mail
 
 def venuebyid(request,id):
     venue_list = Venues.objects.get(id = id)
@@ -16,6 +17,20 @@ def truckbyid(request,id):
 
 
 def index(request):
+    if request.method == 'POST':
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        message = request.POST['message']
+        body = "Name: " + first_name + " " + last_name + "\n" + "\nMessage: " + message
+        send_mail(
+            'Message from ' + first_name + " " + last_name,
+            body,
+            email,
+            ["yashparmar157000@gmail.com"],
+            fail_silently= False
+        )
+        return render(request, 'FoodWagon/index.html', {'name' : first_name + " " + last_name})
     return render(request,'FoodWagon/index.html')
 
 def adminlogin(request):
@@ -28,14 +43,26 @@ def catering(request):
     return render(request,'FoodWagon/catering.html')
 
 def restaurent(request):
+    if request.method == 'POST':
+        first_name = request.POST['first-name']
+        last_name = request.POST['last-name']
+        email = request.POST['email']
+        phone_number = request.POST['phone']
+        message = request.POST['message']
+        body = "Name: " + first_name + " " + last_name + "\nPhone number: "+ phone_number + "\nMassage: " + message
+        send_mail(
+            'Message from ' + first_name + " " + last_name,
+            body,
+            email,
+            ["yashparmar157000@gmail.com"],
+            fail_silently= False
+        )
+        return render(request, 'FoodWagon/restaurent.html', {'name' : first_name + " " + last_name})
     return render(request,'FoodWagon/restaurent.html')
-
 def venue(request):
-    if request.method == "POST":
-        print(request.POST['city'])
-        print(request.POST['sort'])
     venue_list = Venues.objects.all()
     venue_lis = Venues.objects.all()
+
     if request.method == "POST":
         city = request.POST['city']
         Sor = request.POST['sort']
@@ -49,13 +76,12 @@ def venue(request):
                 venue_list = Venues.objects.order_by('Price_per_Day')
             else:
                 venue_list = Venues.objects.order_by('Price_per_Day').reverse()
+        print(venue_list)
     else:
         venue_list = Venues.objects.all()
+    print(venue_list)
     paginator = Paginator(venue_list, 3) 
-    if request.method == "POST":
-        page = request.POST.get('page')
-    else:
-        page = request.GET.get('page')
+    page = request.GET.get('page')
     try:
         venues = paginator.page(page)
     except PageNotAnInteger:
@@ -88,5 +114,7 @@ def login(request):
 
 def register(request):
     return render(request,'FoodWagon/register.html')
+
+    
 
 
